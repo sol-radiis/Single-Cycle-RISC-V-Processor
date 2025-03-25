6df305d86a66e2c5102fd1ee038493f83b0175d1
sol-radiis <149779404+sol-radiis@users.noreply.github.com>
2025-03-26 02:55:50 +0530

    Update ALU.sv

--- RISC V Single Cycle/ALU.sv
+++ RISC V Single Cycle/ALU.sv
@@ -1,35 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/25/2025 11:59:28 AM
-// Design Name: 
-// Module Name: Processor
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
 module ALU (
     input wire [31:0] a, b,
     input wire [3:0] s,   // Control signal should be 4-bit
@@ -38,7 +7,7 @@ module ALU (
 );
     always_comb begin
         e = 32'b0;
-        cout = 0;  // Default carry 
+        cout = 32'b0;  // Default carry 
         case (s)
             4'b0000: {cout, e} = a + b;   
             4'b1000: {cout, e} = a - b;   
@@ -48,10 +17,8 @@ module ALU (
             4'b0001: e = a << b[4:0];     
             4'b0101: e = a >> b[4:0];    
             4'b1101: e = $signed(a) >>> b[4:0];
-            default: e = 32'b0;               
+            default: e =32'b0;              
         endcase
     end
 endmodule
-
-        
             
