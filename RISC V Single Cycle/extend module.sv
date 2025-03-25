aa120cb705f67204c6d1898872bf3d730d457bc5
sol-radiis <149779404+sol-radiis@users.noreply.github.com>
2025-03-26 02:54:21 +0530

    Update extend module.sv

--- RISC V Single Cycle/extend module.sv
+++ RISC V Single Cycle/extend module.sv
@@ -4,23 +4,16 @@ module extend(
 );
     always_comb begin
         case(instruction[6:0])
-            // I-type (e.g., addi, andi, ori, slti, lw)
-            7'b0010011, 7'b0000011, 7'b1100111: 
+            7'b0010011, 7'b0000011, 7'b1100111: // I-type (addi, lw, jalr)
                 dat1 = {{20{instruction[31]}}, instruction[31:20]};
-            // S-type 
-            7'b0100011: 
+            
+            7'b0100011: // S-type (sw)
                 dat1 = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
             
-            // B-type 
-            7'b1100011: 
+            7'b1100011: // B-type (beq, bne)
                 dat1 = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
 
-            // U-type 
-            7'b0110111, 7'b0010111: 
-                dat1 = {instruction[31:12], 12'b0};  // Zero-extend
-
-            // J-type (e.g., jal)
-            7'b1101111: 
+            7'b1101111: // J-type (jal)
                 dat1 = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
 
             default: 
